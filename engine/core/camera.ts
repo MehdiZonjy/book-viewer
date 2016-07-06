4fda9629bae89e5f57b974ec3858a79c4111dbd4
Mehdi Zonjy <mehdizonjy@hotmail.com>
2016-07-06 06:51:46 +0200

    added getViewBounds to camera

--- engine/core/camera.ts
+++ engine/core/camera.ts
@@ -1,41 +1,46 @@
 import {MatrixHelper}  from '../math';
 import {MovableObject} from './movable-object';
-import {mat3,GLM} from 'gl-matrix';
+import {mat3, GLM} from 'gl-matrix';
 
 //TODO handle canvas size change event
-export class Camera extends MovableObject{
+export class Camera extends MovableObject {
     private mProjection: GLM.IArray;
-   // private mView: GLM.IArray;
-    constructor(canvas: HTMLCanvasElement) {
+    // private mView: GLM.IArray;
+    constructor(private mCanvas: HTMLCanvasElement) {
         super();
-        this.createProjection(canvas.width, canvas.height);
-      //  this.mView = mat3.create();
+        this.createProjection(mCanvas.width, mCanvas.height);
+        //  this.mView = mat3.create();
     }
     /*public reset() {
         mat3.identity(this.mView);
     }*/
-  /*  public translate(delta) {
-        mat3.translate(this.mView, this.mView, delta);
-    }
-    public setTranslate(position) {
-        this.mView[TRANSLATION_X] = position[0];
-        this.mView[TRANSLATION_Y] = position[1];
-    }
-    public getTranslation() {
-        return [this.mView[TRANSLATION_X], this.mView[TRANSLATION_Y]];
-    }
-    public scale(deltaScale) {
-        mat3.scale(this.mView, this.mView, deltaScale);
-    }
-
-    public getScale() {
-        return [this.mView[SCALE_X], this.mView[SCALE_Y]];
-    }*/
-    public get View():GLM.IArray {
+    /*  public translate(delta) {
+          mat3.translate(this.mView, this.mView, delta);
+      }
+      public setTranslate(position) {
+          this.mView[TRANSLATION_X] = position[0];
+          this.mView[TRANSLATION_Y] = position[1];
+      }
+      public getTranslation() {
+          return [this.mView[TRANSLATION_X], this.mView[TRANSLATION_Y]];
+      }
+      public scale(deltaScale) {
+          mat3.scale(this.mView, this.mView, deltaScale);
+      }
+  
+      public getScale() {
+          return [this.mView[SCALE_X], this.mView[SCALE_Y]];
+      }*/
+    public get View(): GLM.IArray {
         return this.Transformations;// mat3.clone(this.mView);
     }
-    public get Projection():GLM.IArray {
+    public get Projection(): GLM.IArray {
         return mat3.clone(this.mProjection);
+    }
+
+    public getViewBounds() {
+        //this.mCanvas.width;
+        return [this.transformPointInverse(0, 0), this.transformPointInverse(this.mCanvas.width, 0), this.transformPointInverse(this.mCanvas.width, this.mCanvas.height), this.transformPointInverse(0, this.mCanvas.height)];
     }
 
 
