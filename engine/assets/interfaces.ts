55b79a73e8e95429516b9e27189722d287baebb8
Mehdi Zonjy <mehdizonjy@hotmail.com>
2016-07-09 03:12:32 +0200

    added doc and changed  load signautre

--- engine/assets/interfaces.ts
+++ engine/assets/interfaces.ts
@@ -1,6 +1,25 @@
 import {AssetEntry} from './asset-entry';
+
+/**
+ * handles the async load of assets 
+ * 
+ * @export
+ * @interface ILoader
+ */
 export interface ILoader{
-    load(assetEntry,...args):Promise<AssetEntry>;
-    unload(asset):boolean;
+    /**
+     * loads an asset asynchronously 
+     * 
+     * @param {any} assetEntry
+     * @param {any} args
+     * @returns {Promise<AssetEntry>}
+     */
+    load(assetEntry,extra:any):Promise<AssetEntry>;
+    /**
+     * abort an asset load request if it's still running or deletes the asset if it's ralready loaded 
+     * 
+     * @param {EntryAsset} asset
+     */
+    unload(asset:AssetEntry);
 
 }
