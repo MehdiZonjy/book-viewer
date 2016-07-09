03e5f599fd055ed7f25e77af9732e64a7421a3d2
Mehdi Zonjy <mehdizonjy@hotmail.com>
2016-07-09 04:40:45 +0200

    renamed DemoGame to BookViewer

--- book-viewer/app.ts
+++ book-viewer/app.ts
@@ -7,20 +7,46 @@ import {CameraViewAnimator} from './camera-view-animator';
 import { FileType, IMAGE_LOADER_TYPE, TEXT_LOADER_TYPE} from '../engine/assets';
 import {CameraViewLimiter} from './camera-view-limiter';
 import {PagesManager} from './pages-manager';
-//var glmatrix = require('gl-matrix');
-//import {vec3} from 'glmatrix';
-export class DemoGame extends BaseGame {
-    //private quad: ColoredSprite;
-    //  private mColorSprites: ColoredSprite[];
-    private fpsCounter: FPSCounter;
-    // private mTexture: Texture;
-    private mTexturedSprites: TexturedSprite[];
-    private mCameraViewAnimator: CameraViewAnimator;
-    private mCameraViewLimiter: CameraViewLimiter;
-    private mPagesManager: PagesManager;
-    //  private mTextureShader: SimpleTextureShader;
 
-    private mHammer;
+
+/**
+ * Book Viewer Application main class
+ * 
+ * @export
+ * @class DemoGame
+ * @extends {BaseGame}
+ */
+export class DemoGame extends BaseGame {
+    /**
+     * 
+     * 
+     * @private
+     * @type {FPSCounter}
+     */
+    private fpsCounter: FPSCounter;
+    
+    /**
+     * 
+     * 
+     * @private
+     * @type {CameraViewAnimator}
+     */
+    private mCameraViewAnimator: CameraViewAnimator;
+    /**
+     * 
+     * 
+     * @private
+     * @type {CameraViewLimiter}
+     */
+    private mCameraViewLimiter: CameraViewLimiter;
+    /**
+     * 
+     * 
+     * @private
+     * @type {PagesManager}
+     */
+    private mPagesManager: PagesManager;
+
 
     constructor() {
         super({});
@@ -43,31 +69,6 @@ export class DemoGame extends BaseGame {
 
     protected draw(deltaTime) {
         this.clearColor(1, 0, 0);
-        //this.mSimpleColorShader.use();
-        //this.mSimpleColorShader.setProjection(this.mCamera.Projection);
-
-        /*   this.mSimpleColorShader.beginDraw(this.mCamera.Projection);
-        
-           for (let i = 0, l = this.mColorSprites.length; i < l; i++) {
-               let sprite = this.mColorSprites[i];
-               //sprite.prepareShader();
-               sprite.draw(this.mSimpleColorShader, this.mCamera.View);
-           }
-           this.mSimpleColorShader.endDraw();
-   
-           if (this.mTexturedSprites) {
-               //this.mSimpleTextureShader.use();
-               //this.mSimpleTextureShader.setProjection(this.mCamera.Projection);
-   
-               this.mSimpleTextureShader.beginDraw(this.mCamera.Projection);
-   
-               //this.mTexturedSprite.prepareShader();
-               for (let i = 0; i < this.mTexturedSprites.length; i++)
-                   this.mTexturedSprites[i].draw(this.mSimpleTextureShader, this.mCamera.View);
-               this.mSimpleTextureShader.endDraw();
-   
-           }
-   */
         this.mPagesManager.draw(this.mSimpleTextureShader ,this.mCamera.Projection, this.mCamera.View);
         this.fpsCounter.draw();
 
@@ -77,5 +78,5 @@ export class DemoGame extends BaseGame {
 }
 
 
-
-window['DemoGame'] = DemoGame;
+if(window)
+window['BookViewer'] = DemoGame;
