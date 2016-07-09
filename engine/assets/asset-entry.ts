59eb0f15137b3481c9d4eddaa76089b343fe23f6
Mehdi Zonjy <mehdizonjy@hotmail.com>
2016-07-09 03:12:07 +0200

    added doc and changed loaderArgs to loaderArg

--- engine/assets/asset-entry.ts
+++ engine/assets/asset-entry.ts
@@ -1,33 +1,37 @@
 export class AssetEntry{
+    /**
+     * holds actual asset data (image,text,audio....) 
+     * 
+     * @type {*}
+     */
     public Asset:any;
+    /**
+     * if set to true, once the asset been loaded, it will be discarded and ignored 
+     * 
+     * @type {boolean}
+     */
     public IsCanceled:boolean;
+    /**
+     * used by asset Loader to store additional info 
+     * such as HTMLImageElement or XMLHttpRequest 
+     * @type {*}
+     */
     public LoaderExtra:any;
-    constructor(public Name: string, public OnAssetLoadedCallback: (asset: AssetEntry) => void, public Loader: Symbol,public LoaderArgs:any[],public Group:string) {
+    /**
+     * Creates an instance of AssetEntry.
+     * 
+     * @param {string} Name. asset name such as its URL
+     * @param {(asset: AssetEntry) => void} OnAssetLoadedCallback 
+     * @param {Symbol} Loader. loader Symbol which will be used to lookup the loader
+     * @param {any} LoaderArg. additional parameter to pass to loader.load()
+     * @param {string} Group. request group name 
+     */
+    constructor(public Name: string, public OnAssetLoadedCallback: (asset: AssetEntry) => void, public Loader: Symbol,public LoaderArg:any,public Group:string) {
     }
 
     public get IsLoaded(){
         return this.Asset!=null;
     }
-
-    
-
-    /*  public get Name() {
-          return this.mName;
-      }
-  
-      public get Asset():T {
-          return this.mAsset;
-      }
-      public set Asset(value:T) {
-          this.mAsset = value;
-      }
-      public get OnAssetLoadedCallback() {
-          return this.mOnAssetLoadedCallback;
-      }
-      public set onAssetLoadedCallback(value) {
-          this.mOnAssetLoadedCallback = value;
-      }*/
-
 
 
 
