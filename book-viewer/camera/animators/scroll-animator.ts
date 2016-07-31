dd325bb4475d1c5218355db2151fb2545513e236
Mehdi Zonjy <mehdizonjy@hotmail.com>
2016-07-31 06:09:53 +0200

    fixed bug where fling doesn't stop at min,max

--- book-viewer/camera/animators/scroll-animator.ts
+++ book-viewer/camera/animators/scroll-animator.ts
@@ -1,13 +1,21 @@
 import {OverScroller} from '../over-scroll';
 import {Camera} from '../../../engine/core';
 import {CameraBounds} from '../misc';
+
+export enum ScrollAnimationTrigger {
+    Fling, Pullback, None
+}
 export class ScrollAnimator {
     private mScroller: OverScroller;
     private mCurrentX: number;
     private mCurrentY: number;
+    private mShouldAnimateX;
+    private mShouldAnimateY;
+    private mScrollAnimationTrigger: ScrollAnimationTrigger;
 
     public constructor(private mCameraBounds: CameraBounds, private mCamera: Camera) {
         this.mScroller = new OverScroller();
+        this.mScrollAnimationTrigger = ScrollAnimationTrigger.None;
     }
 
     public cancelScroll() {
@@ -16,6 +24,7 @@ export class ScrollAnimator {
 
     public fling(velocityX: number, velocityY: number) {
 
+        this.mCameraBounds.update();
 
 
         this.mCurrentX = this.mCameraBounds.CurrentX;// startX;
@@ -24,50 +33,65 @@ export class ScrollAnimator {
         console.log(`currentY ${this.mCurrentY}`);
 
 
-        // If we actually can move, fling the scroller
-        if (this.mCameraBounds.CurrentX != this.mCameraBounds.MaxX || this.mCameraBounds.CurrentY != this.mCameraBounds.MaxY) {
-            this.mScroller.fling(this.mCurrentX, this.mCurrentY, -velocityX, -velocityY, this.mCameraBounds.MinX,
-                this.mCameraBounds.MaxX, this.mCameraBounds.MinY, this.mCameraBounds.MaxY, 0, 0);
-        }
+        this.mScroller.fling(this.mCurrentX, this.mCurrentY, -velocityX, -velocityY, this.mCameraBounds.MinX,
+            this.mCameraBounds.MaxX, this.mCameraBounds.MinY, this.mCameraBounds.MaxY, 0, 0);
+        this.mShouldAnimateX = true;
+        this.mShouldAnimateY = true;
+        this.mScrollAnimationTrigger = ScrollAnimationTrigger.Fling;
     }
     public pullBack() {
-
+        this.mCameraBounds.update();
         this.mCurrentX = this.mCameraBounds.CurrentX;// startX;
         this.mCurrentY = this.mCameraBounds.CurrentY;//startY;
 
-        this.mScroller.springBack(this.mCurrentX, this.mCurrentY, this.mCameraBounds.MinX,
-            this.mCameraBounds.MaxX, this.mCameraBounds.MinY, this.mCameraBounds.MaxY);
+        this.mShouldAnimateX = this.mCurrentX < this.mCameraBounds.MinX || this.mCurrentX > this.mCameraBounds.MaxX
+        this.mShouldAnimateY = this.mCurrentY < this.mCameraBounds.MinY || this.mCurrentY > this.mCameraBounds.MaxY;
+        if (this.mShouldAnimateX || this.mShouldAnimateY) {
+            this.mScroller.springBack(this.mCurrentX, this.mCurrentY, this.mCameraBounds.MinX,
+                this.mCameraBounds.MaxX, this.mCameraBounds.MinY, this.mCameraBounds.MaxY);
+            this.mScrollAnimationTrigger = ScrollAnimationTrigger.Pullback;
 
+        }
+    }
+    public getAnimationTriggerSource(): ScrollAnimationTrigger {
+        return this.mScrollAnimationTrigger;
     }
     public isFinished() {
         return this.mScroller.isFinished();
     }
     public finish() {
         this.mScroller.forceFinished(true)
+        this.mShouldAnimateX=false;
+        this.mShouldAnimateY=false;
+        this.mScrollAnimationTrigger=ScrollAnimationTrigger.None;
     }
     public abort() {
         this.mScroller.abortAnimation();
     }
+    public isActive(): boolean {
+        return this.mShouldAnimateX || this.mShouldAnimateY;
+    }
 
     public update() {
-        if (this.mScroller.isFinished()) {
-            return; // remaining post that should not be handled
-        }
+        //    if (this.mScroller.isFinished()) {
+        ////        return; // remaining post that should not be handled
+        //    }
 
         //ImageView imageView = getImageView();
-        if (this.mScroller.computeScrollOffset()) {
+        const isScrollerActive = this.mScroller.computeScrollOffset()
+        if (this.mShouldAnimateX || this.mShouldAnimateY) {
             console.log('update fling animator');
-            const newX = this.mScroller.isXFinished() ? 0 : this.mScroller.getCurrX();
-            const newY = this.mScroller.isYFinished() ? 0 : this.mScroller.getCurrY();
+            const newX =  /*this.mScroller.isXFinished() ? 0 :*/ this.mScroller.getCurrX();
+            const newY =/* this.mScroller.isYFinished() ? 0 : */this.mScroller.getCurrY();
             let deltaX, deltaY;
-            if (isNaN(newX) || newX == 0) {
+            if (isNaN(newX) || !this.mShouldAnimateX) {
                 deltaX = 0;
             }
             else {
                 deltaX = this.mCurrentX - newX;
 
             }
-            if (isNaN(newY) || newY == 0) {
+            if (isNaN(newY) || !this.mShouldAnimateY) {
                 deltaY = 0;
             }
             else
@@ -83,7 +107,12 @@ export class ScrollAnimator {
             this.mCamera.postTranslation(deltaX, deltaY);
             this.mCurrentX = newX;
             this.mCurrentY = newY;
+            if (!isScrollerActive) {
+                this.mShouldAnimateX = false;// isScrollerActive;
+                this.mShouldAnimateY = false;
+                this.mScrollAnimationTrigger = ScrollAnimationTrigger.None;
 
+            }
         }
     }
 
