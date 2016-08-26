a888a8a4d640e311d59fe02e9e5745e9819e5030
Mehdi Zonjy <mehdizonjy@hotmail.com>
2016-08-26 16:30:58 +0200

    generated bundle exports BookViewer in global scope

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
 module.exports = {
     entry: './book-viewer/app.ts',
     output: {
-        filename: './dist/bundle.js'
+        filename: './dist/bundle.js',
+        libraryTarget: 'var',
+        library: 'BookViewer'
     },
     devtool: 'source-map',
 
     resolve: {
-        extensions: ['', '.ts', '.js', '.json', '.css', '.html','.scss']
+        extensions: ['', '.ts', '.js', '.json', '.css', '.html', '.scss']
     },
-    module: { 
-        loaders: [ 
-           // { test: /\.tsx?$/, loader: 'ts-loader' }
-            { 
-                test: /\.tsx?$/, 
-                loader: 'babel-loader?presets[]=es2015!ts-loader' ,
+    module: {
+        loaders: [
+            // { test: /\.tsx?$/, loader: 'ts-loader' }
+            {
+                test: /\.tsx?$/,
+                loader: 'babel-loader?presets[]=es2015!ts-loader',
                 exclude: /node_modules/
             }
         ],
-    } 
+    }
 }
